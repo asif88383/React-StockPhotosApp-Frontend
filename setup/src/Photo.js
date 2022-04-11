437817c11bc195f43f3ca995e852c2cc0f098fe1
Muhammad Asif <asif88383@gmail.com>
2022-04-11 12:52:48 +0500

    first commit

--- setup/src/Photo.js
+++ setup/src/Photo.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({
+  urls: { regular },  // regular is the url for the image   
+  alt_description,   // alt_description is the description of the image (alt text)
+  likes,            // likes is the number of likes
+  user: {
+    name,           // name is the name of the user
+    portfolio_url,  // portfolio_url is the url to the user's portfolio
+    profile_image: { medium }, // profile_image is the url to the user's profile image
+  },
+}) => {
+  return (
+    
+  )
 }
 
 export default Photo
