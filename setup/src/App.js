0fe29b2789db6cd7bd5a79efd2905d8619bb9312
Muhammad Asif <asif88383@gmail.com>
2022-04-11 12:37:54 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,11 +1,58 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { FaSearch } from 'react-icons/fa'
+
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
+// remove current scroll code
+// set default page to 1
+// setup two useEffects
+// don't run second on initial render
+// check for query value
+// if page 1 fetch images
+// otherwise setPage(1)
+// fix scroll functionality
+
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
+  const [query, setQuery] = useState('')
+  const mounted = useRef(false)
+  const [newImages, setNewImages] = useState(false)
+
+  const fetchImages = async () => {
+    setLoading(true)
+    let url
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+    try {
+      const response = await fetch(url)
+      const data = await response.json()  
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results
+        } else if (query) {
+          return [...oldPhotos, ...data.results]  // spread operator
+        } else {
+          return [...oldPhotos, ...data]
+        }
+      })
+      setNewImages(false)
+      setLoading(false)
+    } catch (error) {
+      setNewImages(false)
+      setLoading(false)
+    }
+  }
+
   return <h2>stock photos starter</h2>
 }
 
